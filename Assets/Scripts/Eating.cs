a4f1ec83cd54ac39af712f714d902988f8152883
Dmitry Feoktistov <65128504+dimokrit@users.noreply.github.com>
2023-01-10 08:10:32 +0300

    Stub is Ready
    Added  all stub functional

---
+++ Assets/Scripts/Eating.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Eating : MonoBehaviour
+{
+    public Text programText;
+    public Dropdown daysDropdown;
+    public TextAsset eatingProgramFile;
+    public string[] oneDayProgram;
+
+    public void Start()
+    {
+        oneDayProgram = eatingProgramFile.text.Split('?');
+    }
+
+    public void OnClickBackButton()
+    {
+        SceneManager.LoadScene("Scenes/StubScene");
+    }
+
+    public void OnChangeDayValue()
+    {
+        var day = daysDropdown.value;
+        programText.text = oneDayProgram[day];
+    }
+
+}
---
+++ Assets/Scripts/StubMenu.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StubMenu : MonoBehaviour
+{
+    public void OnClickEatingButton()
+    {
+        SceneManager.LoadScene("Scenes/EatingScene");
+    }
+
+    public void OnClickTrainingButton()
+    {
+        SceneManager.LoadScene("Scenes/TrainingScene");
+    }
+}
