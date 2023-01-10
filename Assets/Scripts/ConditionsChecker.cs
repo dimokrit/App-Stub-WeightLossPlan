163a99d1a250b7ca47f47d56968483d1c5a13c58
Dmitry Feoktistov <65128504+dimokrit@users.noreply.github.com>
2023-01-10 22:18:16 +0300

    Updating checking conditionals

--- Assets/Scripts/ConditionsChecker.cs
+++ Assets/Scripts/ConditionsChecker.cs
@@ -3,54 +3,40 @@ namespace Firebase.Sample.RemoteConfig {
   using System;
   using System.Threading.Tasks;
   using UnityEngine;
+  using UnityEngine.SceneManagement;
+ 
 
   public
-  class UIHandler : MonoBehaviour {
+  class ConditionsChecker : MonoBehaviour {
     private string logText = "";
     const int kMaxLogSize = 16382;
+    public string link;
     Firebase.DependencyStatus dependencyStatus = Firebase.DependencyStatus.UnavailableOther;
     protected bool isFirebaseInitialized = false;
 
     protected virtual void Start() {
-      Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task => {
-        dependencyStatus = task.Result;
-        if (dependencyStatus == Firebase.DependencyStatus.Available) {
-          InitializeFirebase();
-        } else {
-          Debug.LogError(
-            "Could not resolve all Firebase dependencies: " + dependencyStatus);
-        }
-      });
+      if (PlayerPrefs.HasKey("SavedString")) {
+        link = PlayerPrefs.GetString("SavedString");
+        SceneManager.LoadScene("Scenes/WebViewScene");
+      } else {
+          Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task => {
+            dependencyStatus = task.Result;
+            if (dependencyStatus == Firebase.DependencyStatus.Available)
+              InitializeFirebase();
+            else
+              Debug.LogError("Could not resolve all Firebase dependencies: " + dependencyStatus);
+          });
+      }
     }
+
 
     void InitializeFirebase() {
         System.Collections.Generic.Dictionary<string, object> defaults = new System.Collections.Generic.Dictionary<string, object>();
-        defaults.Add("config_test_string", "default local string");
-        defaults.Add("config_test_int", 1);
-        defaults.Add("config_test_float", 1.0);
-        defaults.Add("config_test_bool", false);
-
         Firebase.RemoteConfig.FirebaseRemoteConfig.DefaultInstance.SetDefaultsAsync(defaults).ContinueWithOnMainThread(task => {
             DebugLog("RemoteConfig configured and ready!");
             isFirebaseInitialized = true;
         });
         FetchDataAsync();
-    }
-
-    public void DisplayData() {
-        DebugLog("Current Data:");
-        DebugLog("config_test_string: " +
-               Firebase.RemoteConfig.FirebaseRemoteConfig.DefaultInstance
-               .GetValue("config_test_string").StringValue);
-        DebugLog("config_test_int: " +
-               Firebase.RemoteConfig.FirebaseRemoteConfig.DefaultInstance
-               .GetValue("config_test_int").LongValue);
-        DebugLog("config_test_float: " +
-               Firebase.RemoteConfig.FirebaseRemoteConfig.DefaultInstance
-               .GetValue("config_test_float").DoubleValue);
-        DebugLog("config_test_bool: " +
-               Firebase.RemoteConfig.FirebaseRemoteConfig.DefaultInstance
-               .GetValue("config_test_bool").BooleanValue);
     }
 
     public void DisplayAllKeys() {
@@ -107,8 +93,15 @@ namespace Firebase.Sample.RemoteConfig {
     }
 
     public void Show() {
-      var s = Firebase.RemoteConfig.FirebaseRemoteConfig.DefaultInstance.GetValue("link").StringValue;
-      Debug.Log(s);
+      link = Firebase.RemoteConfig.FirebaseRemoteConfig.DefaultInstance.GetValue("link").StringValue;
+      DebugLog(link);
+      if (link == "") {
+        SceneManager.LoadScene("Scenes/StubScene");
+      } else {
+        PlayerPrefs.SetString("SavedString", link);
+        PlayerPrefs.Save();
+        SceneManager.LoadScene("Scenes/WebViewScene");
+      }
     }
 
     public void DebugLog(string s) {
